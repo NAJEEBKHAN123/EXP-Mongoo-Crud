638e985f86e8a9506ea2f765a31a22410431e940
NAJEEBKHAN123 <najeebkhanlaku.com>
2024-10-09 16:19:35 +0500

    add error message in catch

--- index.js
+++ index.js
@@ -41,6 +41,9 @@ app.get('/', async(req, res) =>{
     try{
         const users = await User.find()
         res.status(200).json(users)
+    }catch(err){
+        console.log("user fetching error", err)
+        res.status(500).json({ message: "Error fetching users", error: err.message });
     }
 })
 
